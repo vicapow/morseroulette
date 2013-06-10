36f8cba38ce818a2dfdd74f9e0c5eb0b7f809b62
Victor Powell <vicapow@gmail.com>
2013-06-10 16:24:36 -0700

    fixed bugs with sound/css across browser

--- public/js/main.js
+++ public/js/main.js
@@ -6,53 +6,57 @@
 // MIT license
  
 ;(function() {
-    var lastTime = 0;
-    var vendors = ['ms', 'moz', 'webkit', 'o'];
-    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
-        window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
-        window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame'] 
-                                   || window[vendors[x]+'CancelRequestAnimationFrame'];
-    }
- 
-    if (!window.requestAnimationFrame)
-        window.requestAnimationFrame = function(callback, element) {
-            var currTime = new Date().getTime();
-            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
-            var id = window.setTimeout(function() { callback(currTime + timeToCall); }, 
-              timeToCall);
-            lastTime = currTime + timeToCall;
-            return id;
-        };
- 
-    if (!window.cancelAnimationFrame)
-        window.cancelAnimationFrame = function(id) {
-            clearTimeout(id);
-        };
-}());
+  var lastTime = 0
+  var vendors = ['ms', 'moz', 'webkit', 'o']
+  for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+      window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame']
+      window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame'] 
+        || window[vendors[x]+'CancelRequestAnimationFrame']
+  }
+
+  if (!window.requestAnimationFrame)
+      window.requestAnimationFrame = function(callback, element) {
+          var currTime = new Date().getTime()
+          var timeToCall = Math.max(0, 16 - (currTime - lastTime))
+          var id = window.setTimeout(
+            function() { 
+              callback(currTime + timeToCall)
+            }
+          , timeToCall )
+          lastTime = currTime + timeToCall
+          return id
+      }
+  if(!window.cancelAnimationFrame)
+    window.cancelAnimationFrame = function(id) { clearTimeout(id) }
+}())
 
 
 
 $(function(){
   var canvas = $('canvas')[0]
+    , pending = false
+    , isbuzzing = false
+    , x = 0
+    , ctx = canvas.getContext('2d')
+  
   canvas.width = 30
   canvas.height = 300
-  var ctx = canvas.getContext('2d')
-  var pending = false
-  var x = 0
-  var isbuzzing = false
+  
   function startbeep(val){
     if(val !== false) val = true
     if(val === isbuzzing) return
     isbuzzing = val
     if(isbuzzing) $('.pen').addClass('active')
     else $('.pen').removeClass('active')
-    sendingAudio.play() // some noise
+    audio.play() // some noise
     if(isbuzzing) socket.emit('beepstart')
     else socket.emit('beepstop')
   }
   function stopbeep(){
+    console.log('stop beep')
     startbeep(false)
-    sendingAudio.pause()
+    audio.currentTime = 0
+    audio.pause()
   }
   window.startbeep = startbeep
   window.stopbeep = stopbeep
@@ -74,15 +78,38 @@ $(function(){
     }, 10)
   })()
   
-  $('body').on('touchstop mouseup', stopbeep)
+  $('body').on('mouseup touchstop', stopbeep)
   $('.handle').on('mousedown touchstart',startbeep)
   $('.next-button').on('mousedown touchstart', function(){
     socket.emit('next')
   })
   
+  var audio = createBeepSound()
+  var socket = io.connect(window.location.href)
+  socket.on('buzz', setBuzz)
+  
+  function setBuzz(data){
+    if(data.isbuzzing === isbuzzing) return
+    isbuzzing = data.isbuzzing
+    if(isbuzzing) audio.play()
+    else{
+      audio.currentTime = 0
+      audio.pause()
+    }
+    if(isbuzzing) $('.pen').addClass('active')
+    else $('.pen').removeClass('active')
+  }
+  
+  setInterval(function(){
+    $('.dialog-container').toggleClass('active')
+  }, 10000)
+  
+})
+
+function createBeepSound(){
   var data = [] // just an array
   var i = 0
-  while (i<100000) { 
+  while( i < 1000000 ){
     data[i++] = 64 + Math.round(60*Math.sin(i/30)) // left speaker
     data[i++] = 64 + Math.round(60*Math.sin(i/800)) // right speaker
   }
@@ -90,25 +117,8 @@ $(function(){
   wave.header.sampleRate = 44100 // set sample rate to 44KHz
   wave.header.numChannels = 2 // two channels (stereo)
   wave.Make(data)
-  var sendingAudio = new Audio(wave.dataURI)
-  sendingAudio.loop = true
-  sendingAudio.pause();
-  
-  var socket = io.connect(window.location.href)
-  var isbuzzing = false
-  socket.on('buzz', setBuzz)
-  
-  function setBuzz(data){
-    if(data.isbuzzing === isbuzzing) return
-    isbuzzing = data.isbuzzing
-    if(isbuzzing) sendingAudio.play()
-    else sendingAudio.pause()
-    if(isbuzzing) $('.pen').addClass('active')
-    else $('.pen').removeClass('active')
-  }
-  
-  setInterval(function(){
-    $('.dialog-container').toggleClass('active')
-  },2000)
-  
-})
+  var audio = new Audio(wave.dataURI)
+  audio.loop = true
+  audio.pause()
+  return audio
+}
